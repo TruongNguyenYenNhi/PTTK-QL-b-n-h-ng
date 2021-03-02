b9eea103f4f4c8719544b9ed8b231eeb396591bc
sanglqsgu <71993474+sanglqsgu@users.noreply.github.com>
2021-03-02 14:56:51 +0700

    Create User.java

---
+++ hibernate/src/main/java/com/hibernate/entities/User.java
@@ -0,0 +1,52 @@
+package com.hibernate.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity( name = "users" )
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="username", length= 50, nullable = false)
+	private String username;
+	
+	@Column(name="password", length= 50, nullable = false)
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
